Correct bug in anneal_recovery: Formula was wrong

mirisim_tso/effects.py
@@ -99,21 +99,21 @@ def anneal_recovery(original_ramp, t_0, frame, config):
     (nb_integrations, nb_frames, nb_y, nb_x) = original_ramp.shape
 
 
-    alpha1 = 0.005051236714489755 * np.ones((nb_y, nb_x))
+    beta1 = 197.97132 * np.ones((nb_y, nb_x))
     amp1   = 11.600852 * np.ones((nb_y, nb_x))
-    alpha2 = 0.0010083051778396745 * np.ones((nb_y, nb_x))
+    beta2 = 991.76323 * np.ones((nb_y, nb_x))
     amp2   = 0.86786327 * np.ones((nb_y, nb_x))
 
     t = t_0 + np.arange(0, nb_frames) * frame  # Time sampling in seconds
     t = t[:,np.newaxis, np.newaxis]  # Prepare broadcasting
 
-    prefactor1 = amp1 * alpha1
-    prefactor2 = amp2 * alpha2
+    prefactor1 = amp1 * beta1
+    prefactor2 = amp2 * beta2
 
-    ramp_difference_t_0 = prefactor1 * np.exp(-(t_0 + anneal_time) / alpha1) + prefactor2 * np.exp(-(t_0 + anneal_time) / alpha2)
+    ramp_difference_t_0 = prefactor1 * np.exp(-(t_0 + anneal_time) / beta1) + prefactor2 * np.exp(-(t_0 + anneal_time) / beta2)
 
     # We integrate from t_0, need to remove evolution between t_0 and t_i (ramp_difference_t_0)
-    ramp_difference = ramp_difference_t_0 - prefactor1 * np.exp(-(t + anneal_time) * alpha1) - prefactor2 * np.exp(-(t + anneal_time) * alpha2)
+    ramp_difference = ramp_difference_t_0 - prefactor1 * np.exp(-(t + anneal_time) / beta1) - prefactor2 * np.exp(-(t + anneal_time) / beta2)
 
     LOG.debug("anneal_recovery() | ramp shape : {}".format(ramp_difference.shape))
     return ramp_difference

mirisim_tso/scripts/plot_effects.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # LOG = logging.getLogger(__name__)
 
 
-def test_response_drift():
+def plot_response_drift():
     """
     Test response drift effect
 
@@ -54,27 +54,29 @@ def test_response_drift():
 
     plt.savefig("response_drift.png")
 
+    return fig
 
-def test_anneal_recovery():
+
+def plot_anneal_recovery():
     """
     Test response drift effect
 
     :return:
     """
 
-    conf = "post_treatment.ini"
+    config = {'simulations': {'dir': 'MIRI_1Integration', 'overwrite': True},
+              'response_drift': {'active': True},
+              'idle_recovery': {'active': True, 'duration': 1000.},
+              'anneal_recovery': {'active': True, 'time': -0.},
+              'noise': {'active': True}}
 
     frame = 0.19  # seconds
     nb_frames = 25
     flux = 10  # DN/s
-    anneal_time = 0  # s
-    subarray_x_size = 72
-    subarray_y_size = 416
 
     frame_sample = np.arange(nb_frames)
     original_ramp = frame_sample * flux * frame
     time_sample = frame_sample * frame
-    config = mirisim_tso.utils.get_config(conf)
 
     original_ramp = original_ramp[np.newaxis,:,np.newaxis, np.newaxis]
 
@@ -91,26 +93,39 @@ def test_anneal_recovery():
 
     fig = plt.figure()
     ax = fig.add_subplot(2, 1, 1)
+    t_0 = 0
     ramp_difference = mirisim_tso.effects.anneal_recovery(original_ramp, t_0, frame, config)
     ax.plot(frame_sample, np.squeeze(original_ramp), label="Original Ramp")
-    ax.plot(frame_sample, np.squeeze(original_ramp) + np.squeeze(ramp_difference), label="Anneal recovery applied")
+    ax.plot(frame_sample, np.squeeze(original_ramp) + np.squeeze(ramp_difference), label="Anneal recovery applied with t_0={}".format(t_0))
+    ax.set_xlabel("Frame number")
+    ax.set_ylabel("Count [DN]")
     # ax.plot(frame_sample, ramp_difference, label="response drift difference")
     ax.legend()
     ax = fig.add_subplot(2, 1, 2)
     ax.plot([0, 2000], [flux, flux], label="Input flux")
     ax.plot(time, time_flux, label="AR corrected flux")
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Flux [DN/s]")
     ax.legend()
     plt.savefig("anneal.png")
 
+    return fig
 
-def test_idle_recovery():
+
+def plot_idle_recovery():
     """
     Test response drift effect
 
     :return:
     """
 
-    conf = "post_treatment.ini"
+
+    config = {'simulations': {'dir': 'MIRI_1Integration', 'overwrite': 'True'},
+              'response_drift': {'active': 'true'},
+              'idle_recovery': {'active': 'true', 'duration': '1000.'},
+              'anneal_recovery': {'active': 'true', 'time': '-0.'},
+              'noise': {'active': 'true'}}
+
 
     t_0 = 60  # seconds
     frame = 0.19  # seconds
@@ -118,11 +133,10 @@ def test_idle_recovery():
     idle_time = 2000  # s
     flux = 500  # DN/s
 
-    nb_frames = 25
     frame_sample = np.arange(nb_frames)
     original_ramp = frame_sample * flux * frame
     time_sample = frame_sample * frame
-    config = mirisim_tso.utils.get_config(conf)
+
 
 
     original_ramp = original_ramp[np.newaxis,:,np.newaxis, np.newaxis]
@@ -151,10 +165,14 @@ def test_idle_recovery():
     ax.legend()
     plt.savefig("idle_recovery.png")
 
+    return fig
+
 if __name__ == "__main__":
     print("-------------------------------------------------------------------")
     print("|                           SCRIPT PLOT                           |")
     print("-------------------------------------------------------------------")
     #mirisim_tso.utils.init_log(stdout_loglevel="DEBUG", file_loglevel="DEBUG")
-    test_anneal_recovery()
+    plot_anneal_recovery()
+
+    plt.show()
     print( "Anneal recovery plot updated with success")

mirisim_tso/scripts/tso_effects_quantification.py
@@ -202,13 +202,6 @@ plt.ylabel('Amplitude (DN/s)')
 fig.savefig('alpha1_curvefits.png', dpi=300, transparent=True)
 
 
-
-
-
-
-
-
-
 # format           (alpha3,    amplitude3,  DN/s)
 points = np.array([(10327.691, -59.095313,  1995.68), #
                    (6133.6167, -74.831790,  2999.83), #
@@ -230,7 +223,7 @@ signal_new = np.linspace(signal[0], signal[-1], 200)
 beta1_new = FORMS_AVAILABLE[fit_form](signal_new, a_alpha3[0], a_alpha3[1], a_alpha3[2], a_alpha3[3], a_alpha3[4])
 
 
-print("\n • Ampltude 3:")
+print("\n • Amplitude 3:")
 fit_form="poly"
 a_amp3, b = curve_fit(FORMS_AVAILABLE[fit_form], signal, amplitude3)#, bounds=(0, np.inf))
 print_formula(a_amp3, "alpha3_amp", "signal", fit_form)
@@ -238,17 +231,10 @@ signal_new = np.linspace(signal[0], signal[-1], 200)
 amplitude1_new = FORMS_AVAILABLE[fit_form](signal_new, a_amp3[0], a_amp3[1], a_amp3[2], a_amp3[3], a_amp3[4])
 
 
-
-
-
-
-
-
-
 fig = plt.figure(figsize=(20,15))
 fig.suptitle('FITTING FOR RESPONSE DRIFT TIME CONSTANTS', fontsize=16)
 ax = fig.add_subplot(2, 2, 1)
-plt.plot(signal, alpha3, 'o', signal_new, alpha3_new, color='xkcd:navy')
+plt.plot(signal, alpha3, 'o', signal_new, a_alpha3, color='xkcd:navy')
 plt.title('Alpha 1')
 #plt.legend()
 # plt.xlim([0,500])
@@ -257,7 +243,7 @@ plt.xlabel('Signal (DN/s)')
 plt.ylabel('Time constant (s)')
 # -- # -- # -- # -- # -- # -- # -- #
 ax = fig.add_subplot(2, 2, 3)
-plt.plot(signal, amplitude3, 'o', signal_new, amplitude3_new, color='xkcd:navy')
+plt.plot(signal, amplitude3, 'o', signal_new, a_amp3, color='xkcd:navy')
 plt.title('Amlplitude 1')
 #plt.legend()
 # plt.xlim([0,500])